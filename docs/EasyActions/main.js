bcf13b80e4d8a839fb82d5ef9e3c77ae7245436f
xTymezzAlt <fete3712@gmail.com>
2026-07-30 06:55:09 +0200

    main

---
+++ docs/EasyActions/main.js
@@ -0,0 +1,26 @@
+async function getYAML(code,name) {
+return `
+name: ${name}
+
+on:
+  push:
+      branches:
+          -main
+  pull_request:
+      branches: 
+          -main
+  workflow_dispatch:
+
+jobs: 
+   example:
+       runs-on: ubuntu-latest
+
+       steps:
+           - name: Generated Using EzActions
+           run: ${code}
+
+`;
+}
+document.getElementById("gen").addEventListener("onclick", () => {
+    document.getElementById("code").value = getYAML(document.getElementById("codeInput").value, document.getElementById("nameInput").value);
+})
